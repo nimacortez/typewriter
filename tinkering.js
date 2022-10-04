4ddfe440164e661e5d956dcb2f3e16404cf225a6
Nima H <nimahz@Nimas-MacBook-Air.local>
2022-10-03 21:44:25 -0400

    adding letter positions

---
+++ tinkering.js
@@ -0,0 +1,16 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2577
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Monaco;}
+{\colortbl;\red255\green255\blue255;\red246\green246\blue239;\red29\green30\blue26;}
+{\*\expandedcolortbl;;\cssrgb\c97255\c97255\c94902;\cssrgb\c15294\c15686\c13333;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\sl340\partightenfactor0
+
+\f0\fs28\fsmilli14300 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 \shad\shadx0\shady-20\shadr0\shado77 \shadc0 Function multiple(a, b) \{\
+	return a * b;\
+\}\
+\
+Const result = multiple (2, 4);\
+\
+console.log(result);}
